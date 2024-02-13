a24398e60f548a5a6610ec81e6c36722122abfde
imnyima <jessicapicon06@gmail.com>
2024-02-13 01:13:49 +0100

    [+] Clase 2024-02-12 Actualizacion y Borrado de Datos

--- src/BBDD/fp_pro.sql
+++ src/BBDD/fp_pro.sql
@@ -1,9 +1,50 @@
-DROP DATABASE IF EXISTS fp_pro;
-CREATE DATABASE IF NOT EXISTS fp_pro;
+# Cambio de modo
+SET GLOBAL sql_mode = 'STRICT_TRANS_TABLES';
 
-CREATE TABLE docentes(
+DROP DATABASE IF EXISTS fp_pro;
+CREATE DATABASE IF NOT EXISTS fp_pro
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_spanish_ci;
+ 
+USE fp_pro;
+CREATE TABLE docentes (
 	nif CHAR(9) NOT NULL UNIQUE,
     nombre VARCHAR(50) NOT NULL,
-	edad TINYINT NULL,
-    PRIMARY KEY Pk_docentes_nif (nif)
-)ENGINE InnoDB;
+    edad TINYINT NULL,
+    PRIMARY KEY PK_docentes_nif (nif)
+) ENGINE InnoDB;
+ 
+CREATE TABLE alumnos (
+	nif CHAR(9) NOT NULL UNIQUE,
+    nombre VARCHAR(50) NOT NULL,
+    fechanac DATE NOT NULL,
+    pagado BOOLEAN NOT NULL,
+    importe DECIMAL (6,2) NOT NULL,
+    docentes_nif CHAR(9) NOT NULL,
+    PRIMARY KEY PK_alumnos_nif (nif),
+    FOREIGN KEY FK_alumnos_docentes_nif (docentes_nif)
+    REFERENCES docentes (nif)
+) ENGINE InnoDB;
+ 
+DESCRIBE docentes;
+DESCRIBE alumnos;
+
+# Visualizar el modo SQL
+SHOW VARIABLES LIKE 'sql_mode';
+# Cambio de modo
+SET GLOBAL sql_mode = 'STRICT_TRANS_TABLES';
+
+# Cambio global hacerlo en my.cnf o my.ini, ponerlo al final 
+# En windows estará en C:\xampp\mysql
+# sql_mode=STRICT_TRANS_TABLES
+
+INSERT INTO docentes VALUES
+("11223344G", "Iván Rodríguez", 47);
+
+INSERT INTO alumnos VALUES
+("33334444S", "Blanca Soler", "2000-02-10", 1, 4100.50, "11223344G"),
+("55556666A", "Alba Jiménez", "2000-06-10", 1, 4100.50, "11223344G"),
+("11112222G", "Juan Carlos Romero", "2000-06-10", 0, 4100.50, "11223344G");
+
+SELECT * FROM docentes;
+SELECT * FROM alumnos;
--- src/Controller/AlumnosController.php
+++ src/Controller/AlumnosController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 // LA SIGUIENTE LÍNEA SE AÑADE AUTOMÁTICAMENTE AL PONER ALUMNOS(); MÁS ABAJO
 use App\Entity\Alumnos;
 use App\Entity\Docentes;
+use App\Repository\AlumnosRepository;
 // LA SIGUIENTE LÍNEA SE AÑADE AUTOMÁTICAMENTE AL PONER DATETIME(); MÁS ABAJO
 use DateTime;
 // AÑADIMOS LA SIGUIENTE LÍNEA. SE CREA AUTOMÁTICAMENTE SI MÁS ABAJO (EN INSERTARALUMNOS) ESCRIBIMOS SOLO "ENTITYMANAGERINTERFACE"
@@ -67,30 +68,89 @@ class AlumnosController extends AbstractController
         // AÑADIMOS FOREACH
         foreach ($registros as $fila) {
             $alumno = new Alumnos();
-            $alumno->setNif($fila["nif"]);
-            $alumno->setNombre($fila["nombre"]);
+            $alumno->setNif($fila['nif']);
+            $alumno->setNombre($fila['nombre']);
 
-            // TRANSFORMAMOS LA SIGUIENTE LÍNEA EN DATETIME
-            $fecha = new DateTime($fila["fechanac"]);
-            $alumno->setFechanac($fecha);
+            $fecha = new DateTime($fila['fechanac']);
+            $alumno->setfechanac($fecha);
 
-            // SEGUIMOS SETEANDO
-            $alumno->setPagado($fila["pagado"]);
-            $alumno->setImporte($fila["importe"]);
+            $alumno->setPagado($fila['pagado']);
+            $alumno->setImporte($fila['importe']);
 
-            // PARA CLAVE FORÁNEA DE DOCENTES:
             $docente = new Docentes();
             $repoDocentes = $entityManager->getRepository(Docentes::class);
-            $valorNifDocente = $fila["docentes_nif"];
+            $valorNifDocente = $fila['docentes_nif'];
             $docente = $repoDocentes->findOneBy(["nif" => $valorNifDocente]);
             $alumno->setDocentesNif($docente);
-            // LO DE SETDOCENTESNIF SE PUEDE MIRAR EN DOCENTES.PHP CÓMO ESTÁ ESCRITO
 
             $entityManager->persist($alumno);
             $entityManager->flush();
         }
+
         return new Response("<h1>¡TODO CORRECTO! - Alumnos insertados</h1>");
     }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#[Route('/consultar-alumnos-cobrados/{pagado}', name: 'consultar_alumnos_cobrados')]
+    //public function consultarAlumnosCobrados(ManagerRegistry $doctrine, int $pagado): JsonResponse
+    public function consultarAlumnosCobrados(AlumnosRepository $repoAlumnos, int $pagado): Response
+    {
+        // endpoint de ejemplo: http://127.0.0.1:8000/alumnos/consultar-alumnos-cobrados/1
+        // $repoAlumnos = $doctrine->getRepository(Alumnos::class);
+        $alumnos = $repoAlumnos->alumnosCobrados($pagado);
+
+        $json = array();
+        foreach ($alumnos as $alumno) {
+            $json[] = array(
+                "nif" => $alumno->getNif(),
+                "nombre" => $alumno->getNombre(),
+                "importe" => $alumno->getImporte(),
+            );
+        }
+
+        // return new JsonResponse($json);
+
+
+        return $this->render('alumnos/index.html.twig', [
+            'controller_name' => 'AlumnosController',
+            'alumnos' => $alumnos,
+        ]);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[Route('/actualizar-alumno/{nif}/{importe}', name: 'actualizar_alumno')]
+    public function actualizarAlumno(EntityManagerInterface $gestorEntidades, String $nif, float $importe): Response
+    {
+        // Endpoint de ejempo: http://127.0.0.1:8000/alumnos/actualizar-alumno/11112222C/255.50
+        $repoAlumnos = $gestorEntidades->getRepository(Alumnos::class);
+        $alumno = $repoAlumnos->findOneBy(["nif" => $nif]);
+
+        // Si no encuentra el alumno, manda un error...
+        if (!$alumno) {
+            throw $this->createNotFoundException(
+                ('ERROR!!!! No se ha encontrado: ' . $nif)
+            );
+        }
+
+        $alumno->setPagado(1);
+        $alumno->setImporte($importe);
+        $gestorEntidades->flush();
+
+        // En el redirect, podemos pasar un conjunto de parámetros: Ej: ['pagado' => 1, 'otro' => "a"]
+        return $this->redirectToRoute("app_alumnos_consultar_alumnos_cobrados", ['pagado' => 1]);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #[Route('/borrar-alumno/{nif}', name: 'borrar_alumno')]
+    public function borrarAlumno(EntityManagerInterface $gestorEntidades, String $nif): Response
+    {
+        // Endpoint de ejempo: http://127.0.0.1:8000/alumnos/borrar-alumno/11112222C
+        $alumno = $gestorEntidades->getRepository(Alumnos::class)->findOneBy(["nif" => $nif]);
+        $gestorEntidades->remove($alumno);
+        $gestorEntidades->flush();
+
+        return $this->redirectToRoute("app_alumnos_consultar_alumnos_cobrados", ['pagado' => 1]);
+    }
 }
 
